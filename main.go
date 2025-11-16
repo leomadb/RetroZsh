e732576fa7c673a5a6ab5a5ad064281e5d672fd6
Leonardo Madrid. <113271044+leomadb@users.noreply.github.com>
2025-11-15 18:28:48 -0600

    Apple appears

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand/v2"
 	"os"
+
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 )
@@ -10,6 +13,10 @@ import (
 func main() {
 	screen, err := tcell.NewScreen()
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+	if err := screen.Init(); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
@@ -29,9 +36,41 @@ func main() {
 				// Dimensions
 				w, h := screen.Size()
 
-				// Draw the maze
+				// Draw the frame
+				for x := 0; x < w; x++ {
+					screen.SetContent(x, 0, '█', nil, tcell.StyleDefault)
+					screen.SetContent(x, h-1, '█', nil, tcell.StyleDefault)
+				}
+				for y := 0; y < h; y++ {
+					screen.SetContent(0, y, '█', nil, tcell.StyleDefault)
+					screen.SetContent(w-1, y, '█', nil, tcell.StyleDefault)
+				}
+				showApple(screen)
 
+				screen.Show()
+
+				time.Sleep(100 * time.Millisecond)
 			}
 		}
 	}()
+
+	for {
+		event := screen.PollEvent()
+		switch event := event.(type) {
+		case *tcell.EventKey:
+			if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyCtrlC {
+				close(quit)
+				return
+			}
+		}
+	}
+}
+
+func showApple(screen tcell.Screen) {
+	w, h := screen.Size()
+
+	x := rand.IntN(w)
+	y := rand.IntN(h)
+	screen.SetContent(x, y, '🍎', nil, tcell.StyleDefault)
+
 }
